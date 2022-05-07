5d28c3adf85c0defc4977bc9a76ac0027a13c821
Knut Wannheden <knut.wannheden@mobi.ch>
2022-05-07 13:07:29 +0200

    search result as simple table

--- zefix_search.py
+++ zefix_search.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, Response
+import os
 import subprocess
-import ndjson
+
+from dotenv import dotenv_values
+from flask import Flask, render_template, request, flash, Response
+
+config = {**dotenv_values(".env"), **os.environ}
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '412fd539978690defac7489c12e689dbd85c4c034fa42801'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -11,12 +14,14 @@ messages = [{'title': 'Message One',
              'content': 'Message Two Content'}
             ]
 
+
 @app.route("/")
 def index():
     # return "<h1 style='color:blue'>Hello There!</h1>"
     return render_template('index.html', messages=messages)
 
-@app.route('/create/', methods=('GET',)) #, 'POST'))
+
+@app.route('/create/', methods=('GET',))  # , 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
@@ -27,18 +32,23 @@ def create():
         else:
             # messages.append({'title': title, 'content': content})
             # return redirect(url_for('index'))
-            
-            process = subprocess.run(['/usr/bin/rg', '--search-zip', '--text', '--fixed-strings', title, '--no-filename', '--no-line-number', '/home/ubuntu/zefix.tar.zst'], capture_output=True, universal_newlines=True)
-            nl='\n'
+
+            process = subprocess.run(['rg', '--search-zip', '--text', '--fixed-strings', title, '--no-filename', '--no-line-number',
+                                      f'{config["ZEFIX_DIR"]}/../zefix.tar.zst'], capture_output=True, universal_newlines=True)
+            nl = '\n'
             return Response(f'[{process.stdout.strip().replace(nl, ",")}]', mimetype='application/json')
 
-    return render_template('create.html')
+    return render_template('search.html')
+
 
 @app.route('/search/')
 def search():
-    process = subprocess.run(['/usr/bin/rg', '--search-zip', '--text', '--fixed-strings', request.args.get('q'), '--no-filename', '--no-line-number', '/home/ubuntu/zefix.tar.zst'], capture_output=True, universal_newlines=True)
-    nl='\n'
+    process = subprocess.run(
+        ['rg', '--search-zip', '--text', '--fixed-strings', request.args.get('q'), '--no-filename', '--no-line-number',
+         f'{config["ZEFIX_DIR"]}/../zefix.tar.zst'], capture_output=True, universal_newlines=True)
+    nl = '\n'
     return Response(f'[{process.stdout.strip().replace(nl, ",")}]', mimetype='application/json')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
