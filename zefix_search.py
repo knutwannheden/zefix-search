0b02c530d0554528e202f4bd680277bef37b7f72
Knut Wannheden <knut.wannheden@mobi.ch>
2022-05-09 09:37:12 +0200

    escape search terms

--- zefix_search.py
+++ zefix_search.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 from pathlib import Path
 
-import regex
+import regex as re
 from dotenv import dotenv_values
 from flask import Flask, render_template, request
 
@@ -18,7 +18,8 @@ def index():
 
 @app.route('/search')
 def search():
-    query = '\\b' + '[^;]{0,30}'.join([s.strip('"') for s in regex.findall('(".*?"|[^ ,.;]+)', request.args.get('q'))]) + '\\b'
+    query = '\\b' + '[^;]{0,30}'.join(
+        [re.escape(s.strip('"')) for s in re.findall('(".*?"|[^ ,.;]+)', request.args.get('q'))]) + '\\b'
     process = subprocess.Popen(
         ['rg', '--search-zip', '--text', '--no-filename', '--no-line-number', '--ignore-case',
          query, *[str(p) for p in Path(f'{config["ZEFIX_DIR"]}/../').glob('zefix-*.tar.zst')]], stdout=subprocess.PIPE)
