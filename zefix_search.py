3cff8f4fcc5f3e7bd0de6d503626b6774d6c1d47
Knut Wannheden <knut.wannheden@mobi.ch>
2022-05-10 23:14:49 +0200

    remove accidentally added caching

--- zefix_search.py
+++ zefix_search.py
@@ -5,17 +5,10 @@ from pathlib import Path
 import regex as re
 from dotenv import dotenv_values
 from flask import Flask, render_template, request
-from flask_caching import Cache
 
 config = {**dotenv_values(".env"), **os.environ}
-flask_config = {
-    # "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 1 * 60 * 60
-}
+
 app = Flask(__name__)
-app.config.from_mapping(config)
-cache = Cache(app)
 
 
 @app.route("/")
@@ -24,7 +17,6 @@ def index():
 
 
 @app.route('/search')
-@cache.cached(timeout=50)
 def search():
     query = '\\b' + '\\b[^;]{0,30}\\b'.join(
         [re.escape(s.strip('"'), literal_spaces=True) for s in re.findall('(".*?"|[^ ,.;]+)', request.args.get('q'))]) + '\\b'
