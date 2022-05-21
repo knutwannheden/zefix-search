c3845c1ceebca0e4912946b748d502782baef148
Knut Wannheden <knut.wannheden@mobi.ch>
2022-05-21 20:00:53 +0200

    fix handling of quoted terms

--- zefix_search.py
+++ zefix_search.py
@@ -49,8 +49,8 @@ def search2():
     if q.startswith('sql:'):
         query = q[4:]
     else:
-        terms = [s.strip(',') for s in re.findall('(".*?"|[^ ]+)', q)]
-        terms = [f'"{t}"' if '-' in t else t for t in terms]
+        terms = [s.strip(',').strip('"') for s in re.findall('(".*?"|[^ ]+)', q)]
+        terms = [f'"{t}"' for t in terms]
         query = "NEAR(" + ' '.join(terms) + ", 5)"
 
     def generate():
